3baf582fab6a86c2af9f39ccbf7f8da17a1459cb
Victor code <okokonvictor67@gmail.com>
2022-08-29 12:37:14 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-function scrollHeader(){
-    const nav = document.getElementById('header')
+//function scrollHeader(){
+    //const nav = document.getElementById('header')
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
-window.addEventListener('scroll', scrollHeader)
+    //if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+//window.addEventListener('scroll', scrollHeader)
 }
